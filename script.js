51b78dcbb8740e8e1fe8abe86209f99da5c15245
Baba Afrid <babaafrid7@gmail.com>
2023-10-28 18:22:15 +0530

    Js updated

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ document.addEventListener("DOMContentLoaded", function() {
         const P = parseFloat(document.getElementById("P").value);
         const S = parseFloat(document.getElementById("S").value);
 
-        let numerator = L * 1 + T * 0.5 + P * 0.5 + S * 0.25;
-        let denominator = (L > 0 ? 1 : 0) + (T > 0 ? 0.5 : 0) + (P > 0 ? 0.5 : 0) + (S > 0 ? 0.25 : 0);
+        let numerator = L * 0.5 + T * 0.25 + P * 0.25 + S * 0.25;
+        let denominator = (L > 0 ? 0.5 : 0) + (T > 0 ? 0.25 : 0) + (P > 0 ? 0.25 : 0) + (S > 0 ? 0.25 : 0);
 
         if (denominator === 0) {
             resultElement.textContent = "Cannot calculate attendance. All fields are zero.";
